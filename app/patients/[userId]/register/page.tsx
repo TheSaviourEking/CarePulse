0db0d6db075917aff88f9bd5725d96e977e86c11
TheSaviourEking <saviour.eking@outlook.com>
2024-10-24 13:38:27 +0100

    chore(ui): add register form page

--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -8,16 +8,13 @@ const Register = async ({ params: { userId } }: SearchParamProps) => {
     const user = await getUser(userId)
     return (
         <div className="flex h-screen max-h-screen">
-            <section className="remove-scrollbar container my-auto">
-                <div className="sub-container max-w-[496]">
+            <section className="remove-scrollbar container flex-1 flex-col py-10">
+                <div className="sub-container max-w-[860px]">
                     <Image src={'/assets/icons/logo-full.svg'} alt="patient" height={1000} width={1000} className="mb-12 h-10 w-fit" />
 
                     <RegisterForm user={user} />
-
-                    <div className="text-14-regular mt-20 flex justify-between">
-                        <p className="justify-items-center text-dark-600 xl:text-left">&copy; 2024 CarePulse</p>
-                        <Link href={'/?admin=true'} className="text-green-500">Admin</Link>
-                    </div>
+                    
+                    <p className="justify-items-center text-dark-600 xl:text-left copyright py-12">&copy; 2024 CarePulse</p>
                 </div>
             </section>
 
