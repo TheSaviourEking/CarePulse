defe4a8e8bd040b5f1042505fb56d7d09d45358e
TheSaviourEking <saviour.eking@outlook.com>
2024-10-25 12:57:22 +0100

    chore(actions): improve patient registration data schema

--- lib/actions/patient.actions.ts
+++ lib/actions/patient.actions.ts
@@ -1,17 +1,16 @@
 "use server"
 
-import { ID, Query } from "node-appwrite"
-import { users } from "../appwrite.config"
+import { ID, Query } from "node-appwrite";
+import { InputFile } from 'node-appwrite/file';
+import { BUCKET_ID, DATABASE_ID, databases, ENDPOINT, PATIENT_COLLECTION_ID, PROJECT_ID, storage, users } from "../appwrite.config"
 import { parseStringify } from "../utils"
 
 export const createUser = async (user: CreateUserParams) => {
     try {
-        console.log(user, 'creating')
-        console.log(users, 'users')
         const newUser = await users.create(ID.unique(), user.email, user.phone, undefined, user.name)
-        console.log(newUser, 'new user')
 
         return parseStringify(newUser)
+
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (error: any) {
         if (error && error?.code === 409) {
@@ -33,3 +32,25 @@ export const getUser = async (userId: string) => {
         console.log(error)
     }
 }
+
+export const registerPatient = async ({ identificationDocument, ...patient }: RegisterUserParams) => {
+    try {
+        let file;
+
+        if (identificationDocument) {
+            const inputFile = identificationDocument && InputFile.fromBuffer(identificationDocument?.get('blobFile') as Blob, identificationDocument?.get('fileName') as string);
+
+            file = await storage.createFile(BUCKET_ID!, ID.unique(), inputFile);
+        }
+
+        const newPatient = await databases.createDocument(DATABASE_ID!, PATIENT_COLLECTION_ID!, ID.unique(), {
+            identificationDocumentId: file?.$id ? file.$id : null,
+            identificationDocumentUrl: file?.$id ? `${ENDPOINT}/storage/bucket/${BUCKET_ID}/files/${file?.$id}/view?project=${PROJECT_ID}` : null,
+            ...patient
+        });
+
+        return parseStringify(newPatient)
+    } catch (error) {
+        console.error(error)
+    }
+};
