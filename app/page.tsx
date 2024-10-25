c93b9ecd0046fb98958cb859f2dc568c613d708b
TheSaviourEking <saviour.eking@outlook.com>
2024-10-25 19:51:29 +0100

    chore(ui): add admin login modal

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,16 @@
 import PatientForm from "@/components/forms/PatientForm";
+import PasskeyModal from "@/components/PasskeyModal";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Home() {
+export default function Home({ searchParams }: SearchParamProps) {
+  const isAdmin = searchParams.admin === 'true';
+
   return (
     <div className="flex h-screen max-h-screen">
+
+      {isAdmin && <PasskeyModal />}
+
       <section className="remove-scrollbar container">
         <div className="sub-container max-w-[496]">
           <Image src={'/assets/icons/logo-full.svg'} alt="patient" height={1000} width={1000} className="mb-12 h-10 w-fit" />
@@ -18,7 +24,7 @@ export default function Home() {
         </div>
       </section>
 
-      <Image src={'/assets/images/onboarding-img.png'} height={1000} width={1000} alt="doctors"  className="side-img max-w-[50%]"/>
+      <Image src={'/assets/images/onboarding-img.png'} height={1000} width={1000} alt="doctors" className="side-img max-w-[50%]" />
     </div>
   )
 }
