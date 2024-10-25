a0ed20c5cc1758d4821092253860646459993695
TheSaviourEking <saviour.eking@outlook.com>
2024-10-25 17:50:25 +0100

    chore(ui): add appointment booking page

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -1,7 +1,6 @@
 import AppointmentForm from "@/components/forms/AppointmentForm";
 import { getPatient } from "@/lib/actions/patient.actions";
 import Image from "next/image";
-import Link from "next/link";
 
 const NewAppointment = async ({ params: { userId } }: SearchParamProps) => {
     const patient = await getPatient(userId);
@@ -13,7 +12,6 @@ const NewAppointment = async ({ params: { userId } }: SearchParamProps) => {
                     <Image src={'/assets/icons/logo-full.svg'} alt="patient" height={1000} width={1000} className="mb-12 h-10 w-fit" />
 
                     <AppointmentForm type='create' userId={userId} patientId={patient.$id} />
-                    {/* <AppointmentForm type='cancel' userId={userId} patientId={patient.$id} /> */}
 
                     <p className="copyright mt-10 py-12">&copy; 2024 CarePulse</p>
                 </div>
