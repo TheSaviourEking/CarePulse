3f1f30adbf3d65db0f10da18f5dc1e56941c9316
TheSaviourEking <saviour.eking@outlook.com>
2024-10-28 09:57:57 +0100

    chore: add appointment status badge

---
+++ components/StatusBadge.tsx
@@ -0,0 +1,22 @@
+import { STATUSICON } from '@/constants'
+import { cn } from '@/lib/utils'
+import Image from 'next/image'
+
+const StatusBadge = ({ status }: { status: Status }) => {
+    return (
+        <div className={cn('status-badge', {
+            'bg-green-600': status === 'scheduled',
+            'bg-blue-600': status === 'pending',
+            'bg-red-600': status === 'cancelled',
+        })}>
+            <Image src={STATUSICON[status]} alt={status} height={24} width={24} className='h-fit w-3' />
+            <p className={cn('status-badge', {
+                'text-green-500': status === 'scheduled',
+                'text-blue-500': status === 'pending',
+                'text-red-500': status === 'cancelled',
+            })}>{status}</p>
+        </div>
+    )
+}
+
+export default StatusBadge
